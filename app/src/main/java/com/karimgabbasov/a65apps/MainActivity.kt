1ac90d4a69907337cf85368b2af078b2bd325313
Karim Gabbasob <karim.gabbasov@gmail.com>
2021-10-13 23:33:51 +0400

    add activity and fragments

--- app/src/main/java/com/karimgabbasov/a65apps/MainActivity.kt
+++ app/src/main/java/com/karimgabbasov/a65apps/MainActivity.kt
@@ -2,10 +2,39 @@ package com.karimgabbasov.a65apps
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.FragmentTransaction
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), FragmentOwner {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if (savedInstanceState == null) {
+            setContactListFragment()
+        }
+    }
+
+    override fun setContactListFragment() {
+        supportFragmentManager.beginTransaction().add(
+            R.id.fragmentContainer,
+            ContactListFragment.getNewInstance(),
+            CONTACT_LIST_FRAGMENT
+        )
+            .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN).commit()
+    }
+
+    override fun setContactDetailsFragment(id: String) {
+        supportFragmentManager.beginTransaction().replace(
+            R.id.fragmentContainer,
+            ContactDetailFragment.getNewInstance(id),
+            CONTACT_DETAIL_FRAGMENT
+        )
+            .addToBackStack(null)
+            .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN).commit()
+    }
+
+    companion object{
+        private const val CONTACT_DETAIL_FRAGMENT = "ContactDetailFragment"
+        private const val CONTACT_LIST_FRAGMENT = "ContactListFragment"
     }
 }
